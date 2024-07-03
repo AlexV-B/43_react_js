236fc59a551172511a2c58610f7aff082e6845af
AlexV-B <vas681@gmail.com>
2024-07-03 13:06:08 +0200

    lesson_1

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
-
+// Главный корневой файл REACT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <>
