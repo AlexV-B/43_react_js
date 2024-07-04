c99904eec9d4462503999de9a26b961bb2407ecd
AlexV-B <vas681@gmail.com>
2024-07-04 13:03:39 +0200

    add className

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import Lesson01 from './lessons/lesson_01/Lesson01';
 // Главный корневой файл REACT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <>
-  <App />
+    <App />
+    <Lesson01 />
  </>
   
 
